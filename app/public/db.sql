a314ccc62f43073dda35783857169e2a10b9249c
frinica <frida.nicander@chasacademy.se>
2022-01-27 15:43:25 +0100

    Update sql in db.sql to set up database

--- app/public/db.sql
+++ app/public/db.sql
@@ -1,11 +1,20 @@
-CREATE DATABASE todoApp;
+-- Adminer 4.8.1 MySQL 5.5.5-10.6.5-MariaDB-1:10.6.5+maria~focal dump
 
-use todoApp;
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-CREATE TABLE todo (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(20) NOT NULL,
-    body VARCHAR(100) NOT NULL,
-    checked TINYINT(1),
-    PRIMARY KEY (id)
-);
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `todo`;
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `checked` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- 2022-01-27 14:39:58
